aea545f54673c1829167e658d4406ca7c8cd00ae
YIZHUSTC <ustcmark@gmail.com>
2019-03-19 02:23:40 -0700

    Update 3 SELECT from Nobel.sql

--- 3 SELECT from Nobel.sql
+++ 3 SELECT from Nobel.sql
@@ -1,4 +1,4 @@
- -- 1. Change the query shown so that it displays Nobel prizes for 1950. 
+-- 1. Change the query shown so that it displays Nobel prizes for 1950. 
 SELECT *
 FROM   nobel
 WHERE  yr = 1950
